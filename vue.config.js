2754cada9b2c585f32a3f5bc000dd4a8a64fe1f3
meko568 <ahmedelbrdan1971@gmail.com>
2025-04-28 16:07:52 +0100

    vue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
+    transpileDependencies: true,
 });
+module.exports = {
+    publicPath:
+        process.env.NODE_ENV === "production"
+            ? "/vue/" // If your GitHub Pages URL is https://meko568.github.io/vue/
+            : "/", // If you're running locally or using development mode
+};
